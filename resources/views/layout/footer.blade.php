718cffa3e8583a4ffff5f56d3b7e5f78b4f79027
galihs12 <galihanggara475@gmail.com>
2023-07-27 21:15:30 +0700

    bismillah

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,73 @@
+@yield('footer')
+<div class="container-fluid bg-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-md-6">
+                <h1 class="text-primary mb-4"><img class="img-fluid me-2" src="img/logo.png" alt=""
+                        style="width: 45px;">PPTQ SIROJUL MUNIR</h1>
+                <span>Feel Free To Send Us A Message About Your Website Needs
+                    Kami siap membantu Anda dan tim kami siap memberikan solusi terbaik untuk kebutuhan Pendidikan
+                    Anda. Terima kasih telah mempertimbangkan layanan kami.</span>
+            </div>
+            <div class="col-md-6">
+                <h5 class="mb-4">Newsletter</h5>
+                <div class="position-relative">
+                    <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text"
+                        placeholder="Your email">
+                    <button type="button"
+                        class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">Get In Touch</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i>Jalan Pendidikan No. 10, Sri Purnomo, Kalirejo, Sri
+                    Mulyo, Kec. Kalirejo, Kabupaten Lampung Tengah, Lampung 34174</p>
+                <p><i class="fa fa-phone-alt me-3"></i>0856-5874-7338</p>
+                <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">Our Services</h5>
+                <a class="btn btn-link" href="">Pondok Pesantren Tahfidzul Qua'an</a>
+                <a class="btn btn-link" href="">Madrasah Ibtidaiyah</a>
+                <a class="btn btn-link" href="">Madrasah Tsanawiyah</a>
+                <a class="btn btn-link" href="">Madrasah Aliyah</a>
+                <a class="btn btn-link" href="">Madrasah Diniyah</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">Quick Links</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Our Services</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">Follow Us</h5>
+                <div class="d-flex">
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square rounded-circle me-1"
+                        href="https://www.facebook.com/pptqsirojul.munirsripurnomo"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square rounded-circle me-1"
+                        href="https://www.youtube.com/@pptqsirojulmunirsripurnomo6785/videos"><i
+                            class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="https://susilomultidigital.space">SUSILOMULTIDIGITAL</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    Designed By <a href="https://susilomultidigital.space">Galih's</a> Distributed By <a
+                        href="https://susilomultidigital.space">SMD</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
